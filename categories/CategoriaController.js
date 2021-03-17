eed93a5dd19d9442a819c80db99ac69f4bcbd23c
Vinicus <viniciusalcantarabarros@gmail.com>
2021-03-17 15:09:08 -0300

    Realizando alteração no título e no slug

--- categories/CategoriaController.js
+++ categories/CategoriaController.js
@@ -62,4 +62,17 @@ router.post("/categories/delete", (req, res) => {
     }
 })
 
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories")
+    })
+})
+
 module.exports = router;
