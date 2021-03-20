da7c03c0d140ba83276f28ba17b5b16081802051
Vinicus <viniciusalcantarabarros@gmail.com>
2021-03-20 14:23:13 -0300

    resolvendo lógica de paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -82,8 +82,39 @@ router.post("/articles/update", (req, res) => {
         }
     }).then(() => {
         res.redirect("/admin/articles")
-    }).catch(err => {
+    }).catch(() => {
         res.redirect("/")
+    })
+})
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if (isNaN(page) || page <= 1) {
+        offset = 0;
+    }else{
+        offset = (parseInt(page) - 1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order: [['id', 'DESC']]
+    }).then(articles => {
+        var next;
+        if (offset + 4 >= articles.count) {
+            next = false;
+        }else{
+            next = true;
+        }
+
+        var result = {
+            next: next,
+            articles: articles
+        }
+
+        res.json(result)
     })
 })
 
